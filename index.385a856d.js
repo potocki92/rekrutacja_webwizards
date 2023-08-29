1728cacd06c0d3d5481e13e144e8cced19876995
potocki92 <40219157+potocki92@users.noreply.github.com>
2023-08-29 18:01:30 +0000

    Updates

---
+++ index.385a856d.js
@@ -0,0 +1,3 @@
+!function(){let e=document.getElementById("menu-toggle"),t=document.getElementById("header"),l=document.querySelector(".nav");e.addEventListener("click",()=>{e.classList.toggle("active"),t.classList.toggle("show"),l.classList.toggle("show")})}();//# sourceMappingURL=index.385a856d.js.map
+
+//# sourceMappingURL=index.385a856d.js.map
