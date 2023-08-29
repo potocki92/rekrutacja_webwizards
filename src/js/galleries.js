743b783324b25a37082689b8cac38023755fe6eb
potocki92 <40219157+potocki92@users.noreply.github.com>
2023-08-29 19:50:15 +0000

    commit

--- src/js/galleries.js
+++ src/js/galleries.js
@@ -1,8 +1,6 @@
 import images from "./images"
 const wrapper = document.querySelector(".slider__slides")
-let slides = document.getElementsByClassName("slider__slide");
 let navlinks = document.getElementsByClassName("slider__navlink");
-let currentSlide = 0;
 
 const array = [
   {
@@ -26,42 +24,45 @@ const array = [
     "retina": images.image4_retina
   }
 ]
-const markup = (e, index) => `
-  <div class="slider__slide ${index === 0 ? "active" : ""}">
-    <img srcset="${e.image} 1x, ${e.retina}" alt=${e.alt}/>
-  </div>
+const markup = (e) => `
+  <img class="slide" srcset="${e.image} 1x, ${e.retina}" alt=${e.alt}/>
 `
 const items = array.map((e, index) => markup(e, index)).join("")
-wrapper.innerHTML = items
+wrapper.innerHTML += items
 
-document.getElementById("nav-button--next").addEventListener("click", () => {
-  changeSlide(currentSlide + 1);
-});
+document.querySelectorAll(".slider__navlink").forEach(() => {
 
-document.getElementById("nav-button--prev").addEventListener("click", () => {
-  changeSlide(currentSlide - 1);
-});
+  let currentSlide = 0;
+  const slides = document.querySelectorAll(".slide")
 
-function changeSlide(moveTo) {
-  if (moveTo >= slides.length) {
-    moveTo = 0;
-  }
-  if (moveTo < 0) {
-    moveTo = slides.length - 1;
-  }
-
-  slides[currentSlide].classList.toggle("active");
-  navlinks[currentSlide].classList.toggle("active");
-  slides[moveTo].classList.toggle("active");
-  navlinks[moveTo].classList.toggle("active");
-
-  currentSlide = moveTo;
-}
-
-document.querySelectorAll(".slider__navlink").forEach((bullet, bulletIndex) => {
-  bullet.addEventListener("click", () => {
-    if (currentSlide !== bulletIndex) {
-      changeSlide(bulletIndex);
+  slides.forEach(
+    (slide, index) => {
+      slide.style.left = `calc(${index * 100}% + ${index * 100}px)`
     }
-  });
+  )
+
+  const goPrev= () => {
+    currentSlide--
+    sliderImage()
+  }
+
+  const goNext = () => {
+    currentSlide++
+    sliderImage()
+  }
+
+  document.getElementById("nav-button--prev").addEventListener("click", () => {
+    goPrev()
+  })
+  document.getElementById("nav-button--next").addEventListener("click", () => {
+    goNext()
+  })
+  
+  const sliderImage = () => {
+    slides.forEach(
+        (slide) => {
+          slide.style.transform = `translateX(-${currentSlide * 100}%)`
+        }
+    )
+  }
 });
