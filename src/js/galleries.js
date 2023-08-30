36651aaac2d3f6413b727d9157470364cf2fd24f
potocki92 <mateusz.potocki92@gmail.com>
2023-08-30 10:08:46 +0200

    galleries.js
    aktualizacja pliku galleries.js.
    zmieniłem w niej funkcje, które przesuwają zdjęcia oraz zaznaczają nawigacje slidera.

--- src/js/galleries.js
+++ src/js/galleries.js
@@ -1,68 +1,103 @@
-import images from "./images"
-const wrapper = document.querySelector(".slider__slides")
-let navlinks = document.getElementsByClassName("slider__navlink");
+import images from "./images";
 
+// Tworzenie danych dla slajdów
 const array = [
   {
-    "alt": "House",
-    "image": images.image,
-    "retina": images.image_retina
+    alt: "House",
+    image: images.image,
+    retina: images.image_retina,
   },
   {
-    "alt": "Car",
-    "image": images.image2,
-    "retina": images.image2_retina
+    alt: "Car",
+    image: images.image2,
+    retina: images.image2_retina,
   },
   {
-    "alt": "Shoes",
-    "image": images.image3,
-    "retina": images.image3_retina
+    alt: "Shoes",
+    image: images.image3,
+    retina: images.image3_retina,
   },
   {
-    "alt": "Trees",
-    "image": images.image4,
-    "retina": images.image4_retina
-  }
-]
+    alt: "Trees",
+    image: images.image4,
+    retina: images.image4_retina,
+  },
+];
+// Wybór kontenera slajdów i funkcja generująca kod dla pojedynczego slajdu
+const wrapper = document.querySelector(".slider__slides");
 const markup = (e) => `
-  <img class="slide" srcset="${e.image} 1x, ${e.retina}" alt=${e.alt}/>
-`
-const items = array.map((e, index) => markup(e, index)).join("")
-wrapper.innerHTML += items
+  <img class="slide" srcset="${e.image} 1x, ${e.retina} 2x" alt="${e.alt}"/>
+`;
 
-document.querySelectorAll(".slider__navlink").forEach(() => {
+// Wstawienie wygenerowanych slajdów do kontenera
+array.forEach((item) => {
+  wrapper.innerHTML += markup(item);
+});
 
-  let currentSlide = 0;
-  const slides = document.querySelectorAll(".slide")
+// Pobranie wszystkich slajdów oraz nawigacji i przypisanie ich do zmiennej
+const slides = document.querySelectorAll(".slide");
+const navlinks = document.querySelectorAll(".slider__navlink");
 
-  slides.forEach(
-    (slide, index) => {
-      slide.style.left = `calc(${index * 100}% + ${index * 100}px)`
-    }
-  )
 
-  const goPrev= () => {
-    currentSlide--
-    sliderImage()
+// Pobranie przycisków nawigacyjnych
+const prevButton = document.getElementById("nav-button--prev");
+const nextButton = document.getElementById("nav-button--next");
+
+
+// Inicjalizacja bieżącego slajdu
+let currentSlide = 0;
+
+// Ustawienie początkowej pozycji slajdów
+slides.forEach((slide, index) => {
+  slide.style.left = `calc(${index * 100}% + ${index * 100}px)`;
+});
+
+// Funkcja aktualizująca położenie slajdów
+const sliderImage = () => {
+  slides.forEach((slide, i) => {
+    slide.style.transform = `translateX(calc(-${currentSlide * 100}% - ${
+      currentSlide * 100
+    }px))`;
+  });
+};
+
+// Funkcja przesuwająca na wybrany slajd
+const goSlide = (slideIndex) => {
+  currentSlide = slideIndex;
+  sliderImage();
+  console.log(currentSlide);
+};
+
+// Nasłuchiwacz na przycisk poprzedniego slajdu
+prevButton.addEventListener("click", () => {
+  if (currentSlide > 0) {
+    goSlide(currentSlide - 1);
+    activeCircle(currentSlide);
   }
+});
 
-  const goNext = () => {
-    currentSlide++
-    sliderImage()
+// Nasłuchiwacz na przycisk następnego slajdu
+nextButton.addEventListener("click", () => {
+  if (currentSlide < slides.length - 1) {
+    goSlide(currentSlide + 1);
+    activeCircle(currentSlide);
   }
+});
 
-  document.getElementById("nav-button--prev").addEventListener("click", () => {
-    goPrev()
-  })
-  document.getElementById("nav-button--next").addEventListener("click", () => {
-    goNext()
-  })
-  
-  const sliderImage = () => {
-    slides.forEach(
-        (slide) => {
-          slide.style.transform = `translateX(-${currentSlide * 100}%)`
-        }
-    )
-  }
+// Funkcja aktywująca odpowiednią nawigację
+const activeCircle = (slide) => {
+  navlinks.forEach((link, index) => {
+    index === slide
+      ? link.classList.add("active")
+      : link.classList.remove("active");
+  });
+};
+
+// Nasłuchiwacze na nawigację za pomocą kropek
+navlinks.forEach((link, index) => {
+  link.addEventListener("click", () => {
+    currentSlide = index;
+    goSlide(currentSlide);
+    activeCircle(index);
+  });
 });
