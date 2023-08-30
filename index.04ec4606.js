76516e380b8016b8841f94094b8d66ea718cdcc7
potocki92 <mateusz.potocki92@gmail.com>
2023-08-30 10:58:03 +0200

    Updates

---
+++ index.04ec4606.js
@@ -0,0 +1,3 @@
+const e=document.getElementById("contact");e.addEventListener("click",()=>{console.log("adsad")});//# sourceMappingURL=index.04ec4606.js.map
+
+//# sourceMappingURL=index.04ec4606.js.map
